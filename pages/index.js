4e015c4962f2c57274ac06827a64b4441d25c562
jorsh96 <excellence.olatunbosun@decagon.dev>
2021-12-31 10:45:55 +0100

    all features working

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from "../components/Nav"
+import Results from "../components/Results"
+import requests from "../utils/requests"
 
 
-export default function Home() {
+export default function Home({results}) {
   return (
     <div >
       <Head>
@@ -14,6 +16,19 @@ export default function Home() {
       </Head>
       <Header/>
       <Nav />
+      <Results results = {results}/>
     </div>
   )
+}
+
+
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json())
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
